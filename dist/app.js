d81e2c0bc7f1b9d3100c943a2445bedcf4a6d7bc
pjcjonas <pjcjonas@gmail.com>
2023-01-08 11:06:26 +0000

    removed dist from git

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./server");
-var port = 3000;
-server_1.app.listen(port, function () {
-    console.log("Example app listening at http://localhost:".concat(port));
-});
